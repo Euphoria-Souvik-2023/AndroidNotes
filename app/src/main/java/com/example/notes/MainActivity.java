6a90ae8b31c542e8d7e93367cbb9282e28ce5ef3
Techno Exponent <Souvik000762@gmail.com>
2023-03-13 16:16:42 +0530

    First commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton notesButton;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        notesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+               Intent intent=new Intent(getApplicationContext(),Notes.class);
+               startActivity(intent);
+            }
+        });
+    }
+    private void init(){
+        notesButton= (FloatingActionButton) findViewById(R.id.newNote);
+    }
+}
